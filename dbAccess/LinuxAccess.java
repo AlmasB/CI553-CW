86ac190273fe9fb1a9621410ba724476faa00bed
wakdj <j.armstrongwalsh1@uni.brighton.ac.uk>
2023-11-04 15:33:33 +0000

    url path update for linux access

--- dbAccess/LinuxAccess.java
+++ dbAccess/LinuxAccess.java
@@ -1,19 +1,35 @@
 package dbAccess;
 
+
+
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+
 /**
   * Implements management of an mySQL database on Linux.
   * @author  Mike Smith University of Brighton
   * @version 2.0
   */
+
+//O2aZ6542FAJs
 class LinuxAccess extends DBAccess
 {
-  public void loadDriver() throws Exception
-  {
-    Class.forName("org.gjt.mm.mysql.Driver").newInstance();
-  }
+  @SuppressWarnings("deprecation")
+public void loadDriver() throws Exception{
+	 
+		    // Load the MySQL driver
+		    Class.forName("com.mysql.cj.jdbc.Driver").newInstance();	    
+  	}
 
-  public String urlOfDatabase()
-  {
-    return "jdbc:mysql://localhost/cshop?user=root";
+  
+
+  public String urlOfDatabase(){
+    return "jdbc:mysql://jaw120:Hencev8b@jaw120.brighton.domains/jaw120";
+
   }
 }
+
+
+
+
+
