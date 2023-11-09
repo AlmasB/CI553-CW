70b5bacc9c9be4ac92c6079ceacc9ef4968d20f2
CB2017 <CB2017@MOCO109-09.university.brighton.ac.uk>
2023-11-09 14:21:20 +0000

    Added first JUnit class

---
+++ catalogue/BetterBasketTest.java
@@ -0,0 +1,14 @@
+package catalogue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BetterBasketTest {
+
+	@Test
+	void testAddProduct() {
+		fail("Not yet implemented");
+	}
+
+}
