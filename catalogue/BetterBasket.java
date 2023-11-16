06e0111c083f3db4154a44568df6ae2cb9fdd30d
CB2017 <CB2017@MOCO109-11.university.brighton.ac.uk>
2023-11-16 14:44:39 +0000

    Better basket class complete

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -2,11 +2,15 @@ package catalogue;
 
 import java.io.Serializable;
 import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * Write a description of class BetterBasket here.
+ * Improved version of the "Basket" class.
  * 
- * @author  Your Name 
+ * @author  Callum Barnett
  * @version 1.0
  */
 public class BetterBasket extends Basket implements Serializable
@@ -15,10 +19,22 @@ public class BetterBasket extends Basket implements Serializable
   
 
   // You need to add code here
-  
-  public void repeatedItems() {
-	  if(theQuantity > 1) {
-		  
+  @Override
+  public boolean add(Product pr) {
+	  for(Product existingProduct: this) {
+		  if(existingProduct.getProductNum().equals(pr.getProductNum())) {
+			  System.out.println("Product exists");
+			  existingProduct.setQuantity(existingProduct.getQuantity()+ pr.getQuantity());
+			  return true;
+		  }
 	  }
+	  
+	  System.out.println("Product doesn't exist");
+	  super.add(pr);
+	  return true;
+  }
+  
+  public void sortBasket() {
+	  Collections.sort(this, (p1, p2) -> p1.getProductNum().compareTo(p2.getProductNum()));
   }
 }
